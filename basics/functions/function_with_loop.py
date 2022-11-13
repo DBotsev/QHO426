eb7ecfa097460cfbf2a25562a895cdcc23468ff6
danielbotsev <117925870+DBotsev@users.noreply.github.com>
2022-11-13 20:11:10 +0000

    "Added code to demonstrate the use of a user-defined function with a loop.

--- basics/functions/function_with_loop.py
+++ basics/functions/function_with_loop.py
@@ -1,12 +1,4 @@
-def climb_ladder(steps_remaining, steps_crossed):
-  if steps_remaining>steps_crossed:
-    print('"Still some way to go!"')
-  else:
-    print("We are almost there!")
 
-climb_ladder(3, 5)
-
-print()
 def cross_bridge(steps):
   for i in range(0, steps,1):
     print('Crossed step.')
